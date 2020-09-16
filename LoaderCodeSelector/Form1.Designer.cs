a07a91581267d71bc2432957985644c4c3cd3706
wycoder91 <1690210872@qq.com>
2020-09-16 14:18:34 +0800

    选码器

---
+++ LoaderCodeSelector/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace LoaderCodeSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbTest = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbTest
+            // 
+            this.cbTest.FormattingEnabled = true;
+            this.cbTest.Items.AddRange(new object[] {
+            "L： 山推",
+            "D：德工"});
+            this.cbTest.Location = new System.Drawing.Point(12, 24);
+            this.cbTest.Name = "cbTest";
+            this.cbTest.Size = new System.Drawing.Size(121, 20);
+            this.cbTest.TabIndex = 0;
+            this.cbTest.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(348, 168);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "增加项";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbTest;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ LoaderCodeSelector/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoaderCodeSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.cbTest.Text.Contains("L： 山推"))
+                MessageBox.Show("L： 山推");
+            if(this.cbTest.Text.Contains("D：德工"))
+                MessageBox.Show("D：德工");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.cbTest.Items.Add("增加项");
+        }
+    }
+}
